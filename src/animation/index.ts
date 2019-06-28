b9eb2c63877c8596189de00f3904eb808d713374
Aliaksandr Shutau <shutovby@gmail.com>
2019-06-29 01:35:53 +0300

    Optional modules

--- src/animation/index.ts
+++ src/animation/index.ts
@@ -1,6 +1,6 @@
 import {Component} from 'malevic';
-import {plugins as domPlugins} from 'malevic/dom';
-import {plugins as stringPlugins} from 'malevic/string';
+import * as malevicDOM from 'malevic/dom';
+import * as malevicString from 'malevic/string';
 import {AnimationDeclaration} from './declaration';
 import {TimingSpec} from './defs';
 import {setAttributePlugin, setStyleAttributePlugin} from './set-attr-plugins';
@@ -18,10 +18,19 @@ export function animate<T = any>(to?: T, timing?: Partial<TimingSpec>) {
 }
 
 export function withAnimation<T extends Component>(type: T): T {
-    domPlugins.setAttribute.add(type, setAttributePlugin);
-    domPlugins.setAttribute.add(type, setStyleAttributePlugin);
-    stringPlugins.stringifyAttribute.add(type, stringifyAttributePlugin);
-    stringPlugins.stringifyAttribute.add(type, stringifyStyleAttrPlugin);
+    // TODO: Some other way to declare optional modules.
+    /* istanbul ignore next */
+    if (malevicDOM) {
+        const domPlugins = malevicDOM.plugins;
+        domPlugins.setAttribute.add(type, setAttributePlugin);
+        domPlugins.setAttribute.add(type, setStyleAttributePlugin);
+    }
+    /* istanbul ignore next */
+    if (malevicString) {
+        const stringPlugins = malevicString.plugins;
+        stringPlugins.stringifyAttribute.add(type, stringifyAttributePlugin);
+        stringPlugins.stringifyAttribute.add(type, stringifyStyleAttrPlugin);
+    }
 
     return type;
 }
